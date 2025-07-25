51280712f34e6fcd351fb692e03ec3977f897386
Thierry Martinez <thierry.martinez@inria.fr>
2025-07-25 16:22:43 +0200

    Use RandomBranchSelector for pr_calc
    This commit adapts the test for the introduction of branch selectors
    in Graphix, that removes the `pr_calc` argument from
    `simulate_pattern`.
    
    We actually don't need to fix the same branch on the two simulations,
    since the pattern is deterministic.

--- tests/test_sympy_parameter.py
+++ tests/test_sympy_parameter.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pytest
 from graphix import Circuit
+from graphix.branch_selector import RandomBranchSelector
 from numpy.random import Generator
 
 from graphix_symbolic import SympyParameter
@@ -34,18 +35,11 @@ def test_parameter_pattern_simulation(backend, fx_rng: Generator) -> None:
     circuit = Circuit(1)
     circuit.rz(0, alpha)
     pattern = circuit.transpile().pattern
-    # Both simulations (numeric and symbolic) will use the same
-    # seed for random number generation, to ensure that the
-    # explored branch is the same for the two simulations.
-    seed = fx_rng.integers(2**63)
-    result_subs_then_simulate = pattern.subs(alpha, 0.5).simulate_pattern(
-        backend, pr_calc=False, rng=np.random.default_rng(seed)
-    )
-    # Note: pr_calc=False is mandatory since we cannot compute
-    # probabilities on symbolic states; we explore one arbitrary
-    # branch.
+    result_subs_then_simulate = pattern.subs(alpha, 0.5).simulate_pattern(backend, rng=fx_rng)
+    # We cannot compute probabilities on symbolic states; we explore
+    # one arbitrary branch.
     result_simulate_then_subs = pattern.simulate_pattern(
-        backend, pr_calc=False, rng=np.random.default_rng(seed), symbolic=True
+        backend, branch_selector=RandomBranchSelector(pr_calc=False, rng=fx_rng), symbolic=True
     ).subs(alpha, 0.5)
     if backend == "statevector":
         assert np.allclose(result_subs_then_simulate.psi, result_simulate_then_subs.psi)
